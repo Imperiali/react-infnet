c9fb2314696d96085a6055f4d3c1a9cef07177e6
imperiali <igor.imperiali@gmail.com>
2019-01-23 12:17:09 -0200

    Tirando foto

--- aula3/src/Camera.js
+++ aula3/src/Camera.js
@@ -1,11 +1,31 @@
 import React from "react";
+import {View, Button} from "react-native"
 import { RNCamera } from 'react-native-camera';
 
-export default () => (<RNCamera
-	style={{flex:1}}
-	type={RNCamera.Constants.Type.back}
-	flashMode={RNCamera.Constants.FlashMode.on}
-	permissionDialogTitle={'Permission to use camera'}
-	permissionDialogMessage={'We need your permission to use your camera phone'}
-	captureAudio={false}
-/>);
+var camera;
+
+export default () => (
+	<View style={{flex:1}}>
+		<RNCamera
+			ref={(ref) => {
+				camera = ref;
+			}}
+			style={{flex:1}}
+			type={RNCamera.Constants.Type.back}
+			flashMode={RNCamera.Constants.FlashMode.on}
+			permissionDialogTitle={'Permission to use camera'}
+			permissionDialogMessage={'We need your permission to use your camera phone'}
+			captureAudio={false}
+		/>
+		<View >
+			<Button onPress={() => {
+				camera.takePictureAsync({})
+					.then((img) => {
+						alert(JSON.stringify(img))
+					})
+					.catch(err => console.error(err))
+			}}
+					title={"Tira foto"}/>
+		</View>
+	</View>
+);
